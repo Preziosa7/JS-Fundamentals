723d136910d04737f332e16ca98591c89916b165
Precious Agbonkhese <preciousagbonkhese@outlook.com>
2025-05-25 11:51:55 +0200

    Add  6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Define an array containing the three required lines
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use a loop to iterate through the array and print each element
+languages.forEach(line => console.log(line));
+
