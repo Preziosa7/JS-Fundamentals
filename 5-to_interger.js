28d38a0898f8c5530dab46abe3b051a122a19ca2
Precious Agbonkhese <preciousagbonkhese@outlook.com>
2025-05-25 11:37:23 +0200

    Add 5-to_interger.js

---
+++ 5-to_interger.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Define an array with the required strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use a loop to iterate and print each element with a single console.log
+for (const lang of languages) console.log(lang);
